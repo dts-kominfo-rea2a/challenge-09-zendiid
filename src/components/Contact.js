20fbe61ca22c12ce82b0b86c932d9e89766b56c0
Zendi Cahya Y <zendicy@gmail.com>
2022-07-02 15:11:49 +0700

    up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import "./Contact.css"
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div class="Contact">
+            <img class="Contact-photo" src={data.photo} alt="contact"></img>
+            <div class="Contact-detail">
+                <h6>{data.name}</h6>
+                <p>{data.email}</p>
+                <p>{data.phone}</p>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import "./Header.css"
 
 const Header = () => {
     return (
-        <></>
+        <div class="Header">
+            <h1>Call a Friend</h1>
+            <h5>your friendly contact app</h5>
+            <p class="Header-Separator">**********************************************************************************************</p>
+        </div>
     )
 }
 
